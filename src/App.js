7b482d611018b01b427391242b5fe9466cde1c27
TinaSemashko <akateria.satory@gmail.com>
2022-08-26 11:03:45 +0200

    disign

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
 import Main from "./Main.js";
-import GoogleMapReact from "google-map-react";
+import Iframe from "react-iframe";
+import central from "../src/central.svg";
 
 function App() {
-  const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
-
   return (
     <div className="App">
       <div className="container">
-        <div style={{ height: "100vh", width: "100%" }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{
-              key: "",
-            }}
-            defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}
-          >
-            <AnyReactComponent
-              lat={59.955413}
-              lng={30.337844}
-              text="My Marker"
-            />
-          </GoogleMapReact>
+        <div className="row">
+          <div className="col">
+            <Main />
+          </div>
+          <div className="col">
+            <Iframe
+              className="round"
+              title="myPlace"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44389.86744575049!2d-73.94844871607448!3d40.68568021855141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25c75b6a8b821%3A0xf3bc87276691ebdc!2sBedford-Stuyvesant%2C%20Brooklyn%2C%20%C3%89tat%20de%20New%20York%2C%20%C3%89tats-Unis!5e0!3m2!1sfr!2sfr!4v1661249022089!5m2!1sfr!2sfr"
+              width="300"
+              height="225"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></Iframe>
+            <div>
+              <img src={central} className="img-fluid one" />
+            </div>
+          </div>
         </div>
-
-        <Main />
+        <hr className="p-2 hr-style"></hr>
+        <footer>
+          <div className="row pb-5">
+            <div className="col-3">
+              <div className="social-links d-flex justify-content-center">
+                <i className="fab fa-linkedin p-2"></i>
+                <i className="fab fa-twitter p-2"></i>
+                <i className="fab fa-facebook p-2"></i>
+                <i className="fab fa-pinterest p-2"></i>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
   );
 }
 
 export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
--- src/Main.js
+++ src/Main.js
@@ -1,43 +1,30 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-//import GoogleMapReact from "google-map-react";
 
-//const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import "./Main.css";
 
 export default function Main() {
   function handleSubmit() {
     alert("data was submitted");
   }
 
-  /*
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
-*/
   return (
-    <Form className="form border border-3 rounded p-5" onSubmit={handleSubmit}>
-      <Form.Label>Your name</Form.Label>
-      <Form.Control placeholder="Enter your name" />
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Your e-mail</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <>
+      <h1 className="hh mt-5">Reach out to us!</h1>
+      <Form className="form mt-2 ps-5" onSubmit={handleSubmit}>
+        <Form.Control placeholder="Your name*" className="mb-1" />
+        <Form.Group className="mb-1" controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Your email*" />
+        </Form.Group>
+        <Form.Group className="mb-1" controlId="ControlTextarea1">
+          <Form.Control as="textarea" rows={3} placeholder="Your message*" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="ControlTextarea1">
-        <Form.Label>Your message</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Button type="submit" variant="flat">
+          Send message
+        </Button>
+      </Form>
+    </>
   );
 }
